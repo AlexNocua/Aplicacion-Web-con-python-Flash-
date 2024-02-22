2e7c86ff5a63efeb1bfad1bced745c2699fe2837
AlexNocua <nocua68@gmail.com~>
2024-02-21 22:39:19 -0500

    Creacion y estructuracion de carpetas, primeros pasos con sentencias basicas de flask, creacion de entorno virtual, (Front-End login y formulario en bootstrap que aun faltan ajustar )

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,14 +1,16 @@
 // alert('Funcionando')
 
 //Mostar el Registro el registro
+const app_description = document.getElementById('app_description')
 const contenidoLogin = document.getElementById('contenido_login')
 const formRegistro = document.getElementById('register')
 const mostrar_Form = document.getElementById('mostrar_Form').addEventListener('click', function () {
     console.log('ffadsa')
     // formRegistro.classList.toggle('mostrar_Form');
     if (formRegistro.style.display === 'none') {
-        formRegistro.style.display = 'block';
+        formRegistro.style.display = 'flex';
         contenidoLogin.style.display = 'none';
+        app_description.style.display = 'none';
     } else {
         formRegistro.style.display = 'none';
     }
@@ -17,10 +19,11 @@ const mostrar_Form = document.getElementById('mostrar_Form').addEventListener('c
 
 const btn_volverLogin = document.getElementById('btn_volverLogin').addEventListener('click', function () {
 
-    if (formRegistro.style.display === 'block') {
+    if (formRegistro.style.display === 'flex') {
         formRegistro.style.display = 'none';
-        contenidoLogin.style.display = 'block';
+        contenidoLogin.style.display = 'flex';
+        app_description.style.display = 'flex';
     } else {
-        formRegistro.style.display = 'block';
+        formRegistro.style.display = 'flex';
     }
 })
