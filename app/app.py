95391f89a51fbb29399172baec2c598d55ea6da3
bestiven <bbaqueroalonso@gmail.com>
2024-04-25 00:57:06 -0500

    conexion con base de datos mongo

--- app/app.py
+++ app/app.py
@@ -1,14 +1,3 @@
-# # save this as app.py
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello():
-#     return "Hello, World sou alex!"
-#####################################
-# inicializacion base
-#####################################
 
 from flask import Flask, render_template, request , redirect,url_for
 from config import *
@@ -24,11 +13,29 @@ app = Flask(__name__)
 @app.route('/')
 def login():
     # rednerizado con jinja2
-    return render_template('login.html')
+    coleccionPersonas =con_bd['Registro']
+    PersonasRegistradas = coleccionPersonas.find()
+    return render_template('login.html', datos = PersonasRegistradas)
 
+@app.route('/guardar_registro',methods=['POST']) 
+def agregarPersona():
+    personas =con_bd['Registro']
+    
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    correo = request.form['correo']
+    contraseña = request.form['contraseña']
+    configContraseña = request.form['configContraseña']
+
+    if nombre and apellido and correo and contraseña and configContraseña : 
+        persona1 = formulario(nombre,apellido,correo,contraseña,configContraseña)
+        personas.insert_one(persona1.formato_doc())
+        return redirect(url_for('login'))
+    
+    else:
+        return "Error "
+    
 # prueba de funcionamiento y rendereizado de la pagina principal
-
-
 @app.route('/pagina_principal')
 def pagina_principla():
     return render_template('pagina_principal.html')
@@ -44,30 +51,6 @@ def inforPerson():
     return render_template('informacionPersona.html')
 
 
-@app.route('/registro') 
-def index():  
-    coleccionPersonas =con_bd['Registro']
-    PersonasRegistradas = coleccionPersonas.find()
-    return render_template('login.html', datos = PersonasRegistradas)
-
-
-@app.route('/guardar_personas',methods=['POST']) 
-def agregarPersona():
-    personas =con_bd['personas']
-    nombre = request.form['nombre']
-    usuario = request.form['usuario']
-    residencia = request.form['residencia']
-    contraseña = request.form['contraseña']
-    configContraseña= request.form['configContraseña']
-
-
-    if nombre and usuario and residencia and contraseña and configContraseña: 
-        persona1 = formulario(nombre,residencia,contraseña,configContraseña)
-        personas.insert_one(persona1.formato_doc())
-        return redirect(url_for('index'))
-    
-    else:
-        return "Error "
-
+   
 if __name__ == '__main__':
     app.run(debug=True)
