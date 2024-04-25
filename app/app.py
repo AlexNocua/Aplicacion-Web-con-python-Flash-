42cfe44d8e772e7f811109ef9c9e44fbfc583add
bestiven <bbaqueroalonso@gmail.com>
2024-04-25 00:22:02 -0500

    cambios de experimento

--- app/app.py
+++ app/app.py
@@ -1,38 +1,15 @@
-# # save this as app.py
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello():
-#     return "Hello, World sou alex!"
-#####################################
-# inicializacion base
-#####################################
-
 from flask import Flask, render_template, request , redirect,url_for
 from config import *
 from ClaseFormulario import formulario
 
 
-con_bd =conexion()
 
-# definicion de archivo principal
 app = Flask(__name__)
-
-
-@app.route('/')
-def login():
-    # rednerizado con jinja2
-    return render_template('login.html')
-
-# prueba de funcionamiento y rendereizado de la pagina principal
-
 
 @app.route('/pagina_principal')
 def pagina_principla():
-    return render_template('pagina_principal.html')
 
+    return render_template('pagina_principal.html')
 
 @app.route('/publicacion')
 def agregarUser():
@@ -43,31 +20,6 @@ def agregarUser():
 def inforPerson():
     return render_template('informacionPersona.html')
 
-
-@app.route('/registro') 
-def index():  
-    coleccionPersonas =con_bd['Registro']
-    PersonasRegistradas = coleccionPersonas.find()
-    return render_template('login.html', datos = PersonasRegistradas)
-
-
-@app.route('/guardar_personas',methods=['POST']) 
-def agregarPersona():
-    personas =con_bd['personas']
-    nombre = request.form['nombre']
-    usuario = request.form['usuario']
-    residencia = request.form['residencia']
-    contraseña = request.form['contraseña']
-    configContraseña= request.form['configContraseña']
-
-
-    if nombre and usuario and residencia and contraseña and configContraseña: 
-        persona1 = formulario(nombre,residencia,contraseña,configContraseña)
-        personas.insert_one(persona1.formato_doc())
-        return redirect(url_for('index'))
-    
-    else:
-        return "Error "
 
 if __name__ == '__main__':
     app.run(debug=True)
